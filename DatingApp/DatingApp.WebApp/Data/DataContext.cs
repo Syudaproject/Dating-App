a711f50f598a3057f7315240ce50f382995b7095
Septian Yuda Prakasa <septian.prakasa@lps.go.id>
2020-09-20 19:40:24 +0700

    Initial commit

---
+++ DatingApp/DatingApp.WebApp/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.WebApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        //public DbSet<> MyProperty { get; set; }
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    builder.Entity<Language>(entity => {
+        //        entity.ToTable("MyAccountsTable");
+        //    });
+        //}
+    }
+}
